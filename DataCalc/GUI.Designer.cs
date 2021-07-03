8b5503ebf869d538d595f8d48e6f446dc4de057f
zDany01 <85403430+zDany01@users.noreply.github.com>
2021-07-04 00:32:04 +0200

    Added info form and optimized code

--- DataCalc/GUI.Designer.cs
+++ DataCalc/GUI.Designer.cs
@@ -37,6 +37,12 @@ namespace CalcoloVelocitaDownload
             this.label2 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.textBox3 = new System.Windows.Forms.TextBox();
+            this.FormMenu = new System.Windows.Forms.MenuStrip();
+            this.AppStripContainer = new System.Windows.Forms.ToolStripMenuItem();
+            this.ReimpostaDatiToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.EsciToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.InfoToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+            this.FormMenu.SuspendLayout();
             this.SuspendLayout();
             // 
             // comboBox1
@@ -46,7 +52,7 @@ namespace CalcoloVelocitaDownload
             "TebiByte",
             "GibiByte",
             "MebiByte"});
-            this.comboBox1.Location = new System.Drawing.Point(129, 42);
+            this.comboBox1.Location = new System.Drawing.Point(126, 68);
             this.comboBox1.Name = "comboBox1";
             this.comboBox1.Size = new System.Drawing.Size(97, 21);
             this.comboBox1.TabIndex = 1;
@@ -55,7 +61,7 @@ namespace CalcoloVelocitaDownload
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(9, 7);
+            this.label1.Location = new System.Drawing.Point(6, 33);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(225, 32);
             this.label1.TabIndex = 2;
@@ -64,7 +70,7 @@ namespace CalcoloVelocitaDownload
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.textBox1.Location = new System.Drawing.Point(9, 42);
+            this.textBox1.Location = new System.Drawing.Point(6, 68);
             this.textBox1.MaxLength = 16;
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(119, 21);
@@ -73,7 +79,7 @@ namespace CalcoloVelocitaDownload
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.25F);
-            this.textBox2.Location = new System.Drawing.Point(7, 119);
+            this.textBox2.Location = new System.Drawing.Point(4, 145);
             this.textBox2.MaxLength = 16;
             this.textBox2.Name = "textBox2";
             this.textBox2.Size = new System.Drawing.Size(113, 21);
@@ -90,7 +96,7 @@ namespace CalcoloVelocitaDownload
             "MegaBit(Mb/s)",
             "KibiByte(KiB/s)",
             "KiloBit(Kb/s)"});
-            this.comboBox2.Location = new System.Drawing.Point(121, 119);
+            this.comboBox2.Location = new System.Drawing.Point(118, 145);
             this.comboBox2.Name = "comboBox2";
             this.comboBox2.Size = new System.Drawing.Size(106, 21);
             this.comboBox2.TabIndex = 4;
@@ -99,7 +105,7 @@ namespace CalcoloVelocitaDownload
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(4, 84);
+            this.label2.Location = new System.Drawing.Point(1, 110);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(215, 32);
             this.label2.TabIndex = 6;
@@ -107,29 +113,70 @@ namespace CalcoloVelocitaDownload
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(7, 147);
+            this.button1.Location = new System.Drawing.Point(4, 173);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(220, 23);
             this.button1.TabIndex = 7;
             this.button1.Text = "Calcola";
             this.button1.UseVisualStyleBackColor = true;
-            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
             // 
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox3.Location = new System.Drawing.Point(7, 177);
+            this.textBox3.Location = new System.Drawing.Point(4, 203);
             this.textBox3.Multiline = true;
             this.textBox3.Name = "textBox3";
             this.textBox3.ReadOnly = true;
             this.textBox3.Size = new System.Drawing.Size(220, 77);
             this.textBox3.TabIndex = 8;
             // 
+            // FormMenu
+            // 
+            this.FormMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.AppStripContainer,
+            this.InfoToolStripMenuItem1});
+            this.FormMenu.Location = new System.Drawing.Point(0, 0);
+            this.FormMenu.Name = "FormMenu";
+            this.FormMenu.Size = new System.Drawing.Size(231, 24);
+            this.FormMenu.TabIndex = 9;
+            this.FormMenu.Text = "menuStrip1";
+            // 
+            // AppStripContainer
+            // 
+            this.AppStripContainer.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.ReimpostaDatiToolStripMenuItem,
+            this.EsciToolStripMenuItem});
+            this.AppStripContainer.Name = "AppStripContainer";
+            this.AppStripContainer.Size = new System.Drawing.Size(41, 20);
+            this.AppStripContainer.Text = "App";
+            // 
+            // ReimpostaDatiToolStripMenuItem
+            // 
+            this.ReimpostaDatiToolStripMenuItem.Name = "ReimpostaDatiToolStripMenuItem";
+            this.ReimpostaDatiToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.ReimpostaDatiToolStripMenuItem.Text = "Reimposta Dati";
+            this.ReimpostaDatiToolStripMenuItem.Click += new System.EventHandler(this.ReimpostaDatiToolStripMenuItem_Click);
+            // 
+            // EsciToolStripMenuItem
+            // 
+            this.EsciToolStripMenuItem.Name = "EsciToolStripMenuItem";
+            this.EsciToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.EsciToolStripMenuItem.Text = "Esci";
+            this.EsciToolStripMenuItem.Click += new System.EventHandler(this.EsciToolStripMenuItem_Click);
+            // 
+            // InfoToolStripMenuItem1
+            // 
+            this.InfoToolStripMenuItem1.Name = "InfoToolStripMenuItem1";
+            this.InfoToolStripMenuItem1.Size = new System.Drawing.Size(24, 20);
+            this.InfoToolStripMenuItem1.Text = "?";
+            this.InfoToolStripMenuItem1.Click += new System.EventHandler(this.InfoToolStripMenuItem1_Click);
+            // 
             // GUIForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(231, 259);
+            this.ClientSize = new System.Drawing.Size(231, 293);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.label2);
@@ -138,11 +185,15 @@ namespace CalcoloVelocitaDownload
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.FormMenu);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.MainMenuStrip = this.FormMenu;
             this.MaximizeBox = false;
             this.Name = "GUIForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Calcolo velocità download";
+            this.FormMenu.ResumeLayout(false);
+            this.FormMenu.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -158,6 +209,11 @@ namespace CalcoloVelocitaDownload
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.MenuStrip FormMenu;
+        private System.Windows.Forms.ToolStripMenuItem AppStripContainer;
+        private System.Windows.Forms.ToolStripMenuItem ReimpostaDatiToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem EsciToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem InfoToolStripMenuItem1;
     }
 }
 
--- DataCalc/GUI.cs
+++ DataCalc/GUI.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CalcoloVD;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,7 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
+//EID = Event In Designer
 namespace CalcoloVelocitaDownload
 {
     public partial class GUIForm : Form
@@ -17,17 +18,22 @@ namespace CalcoloVelocitaDownload
             InitializeComponent();
             textBox1.TextChanged += TextBoxNotNumber;
             textBox2.TextChanged += TextBoxNotNumber;
-            comboBox1.SelectedIndex = 1; comboBox2.SelectedIndex = 3; //default
+            ResetData();
         }
 
+        private void ResetData()
+        {
+            comboBox1.SelectedIndex = 1; comboBox2.SelectedIndex = 3;
+            textBox1.Clear(); textBox2.Clear(); textBox3.Clear();
+        }
 
-        private void button1_Click(object sender, EventArgs e)
+        private void Button1_Click(object sender, EventArgs e) //EID
         {
             if (textBox1.Text == "" || textBox2.Text == "" || textBox1.Text == "." || textBox2.Text == "." || textBox1.Text == "," || textBox2.Text == ",") { return; } // | e || sono rispettivamente or e orElse, nel caso in cui il primo sia vero allora con ||(orElse) il secondo non verrà neanche valutato. analogamente questo vale anche per & e && (and e ?andElse?)
             decimal QuantitaDatiDaScaricare = decimal.Parse(textBox1.Text.Replace(".",",")); //cambiare il punto in virgola serve al programma per far in modo che prenda il valore come decimal
             decimal VelocitaDwl = decimal.Parse(textBox2.Text.Replace(".",","));
 
-            if (!(QuantitaDatiDaScaricare > 0) || !(VelocitaDwl > 0)) { return; }
+            if ((QuantitaDatiDaScaricare <= 0) || (VelocitaDwl <= 0)) { return; }
             //0 TeraByte, 1 Gigabyte, 2 Megabyte
             if (comboBox1.SelectedIndex == 0)
             {
@@ -57,8 +63,17 @@ namespace CalcoloVelocitaDownload
 
             int ore = (isec / 3600); isec %= 3600;//explicit conversion to int
 
-            int minuti = (isec / 60); isec =Convert.ToInt32(secondi % 86400 % 3600 % 60); //usato nel caso in cui il numero superi {sec}.5 è quindi viene automaticamente arrotondato a {sec}++ grazie al convert.ToInt32, non provare a semplificare nella parentesi apparente bug?
+            int minuti = (isec / 60); isec =Convert.ToInt32(secondi % 86400 % 3600 % 60); //usato nel caso in cui il numero superi {sec}.5 è quindi viene automaticamente arrotondato a {sec}++ grazie al convert.ToInt32.
             return $"{giorni} giorni {ore} ore {minuti} minuti {isec} secondi.";
+        }
+
+        private void EsciToolStripMenuItem_Click(object sender, EventArgs e) {Application.Exit();} //EID
+
+        private void ReimpostaDatiToolStripMenuItem_Click(object sender, EventArgs e) {ResetData();} //EID
+
+        private void InfoToolStripMenuItem1_Click(object sender, EventArgs e) //EID
+        {
+            new InfoApp().ShowDialog(); //show modal non permette di ritornare alla finestra principale se prima non si è chiusa quella appena aperta facendo così l'utente non può creare finestre infinite.
         }
     }
 }
